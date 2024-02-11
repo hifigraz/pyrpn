387dad76c606b95ff0d3350bd7f80cc98a3f271e
Robert Ulmer <robert@familieulmer.info>
2024-02-11 11:53:44 +0100

    add pop and push

--- pyrpn/_stack.py
+++ pyrpn/_stack.py
@@ -2,7 +2,23 @@ from typing import List
 
 
 class StackElement:
-    pass
+    def __str__(self) -> str:
+        return '"StackElement"'
+
+
+class StackNumberElement(StackElement):
+    def __init__(self, number: StackElement | int | float):
+        if type(number) is int:
+            self._data = float(number)
+        elif type(number) is float:
+            self._data = number
+        elif isinstance(number, StackNumberElement):
+            self.data = number._data
+        else:
+            raise AttributeError("Invalid argument (%s)" % str(number))
+
+    def __str__(self) -> str:
+        return str(self._data)
 
 
 class Stack:
@@ -11,8 +27,32 @@ class Stack:
 
     def push_back(self, stack_element: StackElement) -> None:
         if not isinstance(stack_element, StackElement):
-            raise AttributeError("Invalid argument")
+            raise AttributeError("Invalid argument (%s)" % str(stack_element))
         self._data.append(stack_element)
+
+    def push_front(self, stack_element: StackElement) -> None:
+        if not isinstance(stack_element, StackElement):
+            raise AttributeError("Invalid argument (%s)" % str(stack_element))
+        self._data = [stack_element] + self._data
+
+    def pop_front(self) -> StackElement:
+        try:
+            return self._data.pop(0)
+        except IndexError:
+            raise IndexError("pop from empty stack")
+
+    def pop_back(self) -> StackElement:
+        try:
+            return self._data.pop(-1)
+        except IndexError:
+            raise IndexError("pop from empty stack")
 
     def __len__(self) -> int:
         return len(self._data)
+
+    def __str__(self) -> str:
+        return_value = "[%s ]"
+        return_value = return_value % ", ".join(
+            map(lambda x: str(x), self._data)
+        )
+        return return_value
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -18,5 +18,68 @@ def test_basic_stack_functions():
     tmp_stack.push_back(stack.StackElement())
     assert len(tmp_stack) == 1
 
-    with pytest.raises(AttributeError, match="Invalid argument"):
+    with pytest.raises(AttributeError, match="Invalid argument [(]1[)]"):
         tmp_stack.push_back(1)  # pyright: ignore[reportGeneralTypeIssues]
+    with pytest.raises(AttributeError, match="Invalid argument [(]Robert[)]"):
+        tmp_stack.push_back(
+            "Robert"  # pyright: ignore[reportGeneralTypeIssues]
+        )
+    assert str(tmp_stack) == '["StackElement" ]'
+
+    with pytest.raises(AttributeError, match="Invalid argument [(]1[)]"):
+        tmp_stack.push_front(1)  # pyright: ignore[reportGeneralTypeIssues]
+    with pytest.raises(AttributeError, match="Invalid argument [(]Robert[)]"):
+        tmp_stack.push_front(
+            "Robert"  # pyright: ignore[reportGeneralTypeIssues]
+        )
+    tmp_stack.push_front(stack.StackElement())
+    assert str(tmp_stack) == '["StackElement", "StackElement" ]'
+
+
+def test_stack_number_element():
+    temp_stack = stack.Stack()
+    temp_stack.push_back(stack.StackNumberElement(1))
+    assert str(temp_stack == "[1.0 ]")
+    temp_stack.push_back(stack.StackNumberElement(1.1))
+    assert len(temp_stack) == 2
+    assert str(temp_stack == "[1.0, 1.1 ]")
+    temp_element = stack.StackNumberElement(2.5)
+    assert str(temp_element) == "2.5"
+    temp_stack.push_back(temp_element)
+    assert len(temp_stack) == 3
+    assert str(temp_stack) == "[1.0, 1.1, 2.5 ]"
+    temp_stack.push_back(stack.StackNumberElement(pow(10.5, 100)))
+    assert len(temp_stack) == 4
+    assert str(temp_stack) == "[1.0, 1.1, 2.5, 1.3150125784630346e+102 ]"
+    temp_stack.push_front(stack.StackNumberElement(1.0 / 4.0))
+    assert len(temp_stack) == 5
+    assert str(temp_stack) == "[0.25, 1.0, 1.1, 2.5, 1.3150125784630346e+102 ]"
+
+
+def test_stack_pop_functions():
+    temp_stack = stack.Stack()
+    assert str(temp_stack) == "[ ]"
+    temp_stack.push_back(stack.StackNumberElement(1.5))
+    temp_stack.push_back(stack.StackNumberElement(2.5))
+    temp_stack.push_back(stack.StackNumberElement(3.5))
+
+    stack_element = temp_stack.pop_front()
+    assert isinstance(stack_element, stack.StackElement)
+    assert str(stack_element) == "1.5"
+    assert str(temp_stack) == "[2.5, 3.5 ]"
+    assert len(temp_stack) == 2
+    stack_element = temp_stack.pop_back()
+    assert isinstance(stack_element, stack.StackElement)
+    assert str(stack_element) == "3.5"
+    assert str(temp_stack) == "[2.5 ]"
+    assert len(temp_stack) == 1
+    stack_element = temp_stack.pop_back()
+    assert isinstance(stack_element, stack.StackElement)
+    assert str(stack_element) == "2.5"
+    assert str(temp_stack) == "[ ]"
+    assert len(temp_stack) == 0
+
+    with pytest.raises(IndexError, match="pop from empty stack"):
+        temp_stack.pop_front()
+    with pytest.raises(IndexError, match="pop from empty stack"):
+        temp_stack.pop_back()
